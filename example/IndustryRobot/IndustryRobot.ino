5a2ba848ed428b0509e01b90880fe6c5edaeb840
Manel <manelruizblas@gmail.com>
2016-05-03 10:28:24 +0200

    Updated
    Changed to LLC
    Deleted zgz.verkstad.cc

--- example/IndustryRobot/IndustryRobot.ino
+++ example/IndustryRobot/IndustryRobot.ino
@@ -11,8 +11,8 @@
 *  parameters so it runs faster along the track.
 *  
 *  This is an example from the Svante robot summer camp.
-*  zgz.verkstad.cc 
-*  (c) 2014 Arduino Verkstad
+*   
+*  (c) 2014 Arduino LLC
 *  
 */
 
