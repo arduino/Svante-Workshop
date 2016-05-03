01269644f5e932a58057a904bb29c3315788c911
Manel <manelruizblas@gmail.com>
2016-05-03 09:46:08 +0200

    Update WiggleYourTail_1.ino

--- example/Servo/WiggleYourTail_1/WiggleYourTail_1.ino
+++ example/Servo/WiggleYourTail_1/WiggleYourTail_1.ino
@@ -6,8 +6,8 @@
  and forth to look like a tail.
  
  This is an example from the Svante robot summer camp.
- zgz.verkstad.cc 
- (c) 2014 Arduino Verkstad
+
+ (c) 2014 Arduino LLC
  */
 
 #include <Svante.h>
