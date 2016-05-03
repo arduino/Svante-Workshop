eb935d872a8d344cfaa541af0977ab18c261c117
Manel <manelruizblas@gmail.com>
2016-05-03 10:07:39 +0200

    Updated
    Changed to LLC
    Deleted zgz.verkstad.cc

--- example/Spin/Spin_1/Spin_1.ino
+++ example/Spin/Spin_1/Spin_1.ino
@@ -5,8 +5,8 @@
  stop 1 second, spin clockwise 1 second and finally stopfor another second.
  
  This is an example from the Svante robot summer camp.
- zgz.verkstad.cc 
- (c) 2014 Arduino Verkstad
+
+ (c) 2014 Arduino LLC
  */
 
 #include <Svante.h>
