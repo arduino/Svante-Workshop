f765efcf5671826c63a0d1e15a0620490b02a5f6
Manel <manelruizblas@gmail.com>
2016-05-03 10:34:23 +0200

    Updated
    Changed to LLC
    Deleted zgz.verkstad.cc

--- example/Puppy/RobotPuppy_3/RobotPuppy_3.ino
+++ example/Puppy/RobotPuppy_3/RobotPuppy_3.ino
@@ -7,8 +7,8 @@
  robot wheels are controlled to move towards the object.
  
  This is an example from the Svante robot summer camp.
- zgz.verkstad.cc 
- (c) 2014 Arduino Verkstad
+
+ (c) 2014 Arduino LLC
  */
  
 #include <Svante.h>
